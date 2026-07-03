cb923ccbf34ce58fd8a098d7ef171aeef01271a0
niahsage <niahsage@gmail.com>
2026-07-03 19:18:45 -0400

    Add CNAME file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import "../styles/navbar.css";
+import logoImg from "../assets/images/niah-sage-logo.jpg";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <Link to="/" className="logo">
-        Niah Mckyton
+        <img src={logoImg} alt="Niah Sage logo" />
       </Link>
 
       <div className="nav-links">
